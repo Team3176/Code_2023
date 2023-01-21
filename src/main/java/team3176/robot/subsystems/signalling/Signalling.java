2f0af6b9cb7f9f308e595441f4ba6b02b5797a16
char3176 <char3176@gmail.com>
2023-01-21 15:52:49 -0500

    LED code to be manipulated and fixed

--- src/main/java/team3176/robot/subsystems/signalling/Signalling.java
+++ src/main/java/team3176/robot/subsystems/signalling/Signalling.java
@@ -5,17 +5,46 @@
 package team3176.robot.subsystems.signalling;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 
 public class Signalling extends SubsystemBase {
   
   private static Signalling instance = new Signalling();
+  private AddressableLED m_led;
+  private AddressableLEDBuffer m_ledBuffer;
+
+  private int m_rainbowFirstPixelHue;
 
   
   /**
    * Creates the default references for VisionClient, specifically for Limelight values
    */
   public Signalling(){
+    m_led = new AddressableLED(9);
+    m_ledBuffer = new AddressableLEDBuffer(60);
+    m_led.setLength(m_ledBuffer.getLength());
 
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+
+  }
+
+  private void rainbow() {
+    // For every pixel
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Calculate the hue - hue is easier for rainbows because the color
+      // shape is a circle so only one value needs to precess
+      final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+      // Set the value
+      m_ledBuffer.setHSV(i, hue, 255, 128);
+    }
+    // Increase by to make the rainbow "move"
+    m_rainbowFirstPixelHue += 3;
+    // Check bounds
+    m_rainbowFirstPixelHue %= 180;
+    
+    m_led.setData(m_ledBuffer);
   }
 
   public static Signalling getInstance(){
