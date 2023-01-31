27b2367fe3054b2a4986c7fd62468a55ae0ae9e9
char@odin <char3176@gmail.com>
2023-01-31 00:34:43 -0500

    Beginning to build RobotState class.

---
+++ src/main/java/team3176/robot/subsystems/RobotState.java
@@ -0,0 +1,100 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package team3176.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import org.littletonrobotics.junction.Logger;
+
+import team3176.robot.subsystems.RobotStateIO; 
+import team3176.robot.subsystems.RobotStateIO.RobotStateIOInputs; 
+
+
+
+public class RobotState extends SubsystemBase {
+
+  private final RobotStateIO io;
+  private final RobotStateIOInputs inputs = new RobotStateIOInputs();
+  private static RobotState instance;
+
+  enum e_ClawPositionState {
+    OPEN,
+    CLOSED,
+    IDLE
+  }
+
+  enum e_ClawRollersState {
+    PosSPIN,  //Means front-most side of rollers spinning INTO the Claw, toward it's center 
+    NegSPIN,  //Means front-most side of rollers spinning OUT-OF the Claw, toward it's exterior
+    NoSPIN,  //Means front-most rollers are not spinning
+  }
+
+  enum e_IntakePositionState {
+    EXTENDED,
+    RETRACTED
+  }
+
+  enum e_IntakeDetectsPossessionOf {
+    CONE,
+    CUBE,
+    NOTHING,
+    ERROR
+  }
+
+  enum e_IntakeMotorState {
+    PosSPIN,  //Means front-most side of rollers spinning INTO the Intake, toward it's center 
+    NegSPIN,  //Means front-most side of rollers spinning OUT-OF the Intake, toward it's exterior
+    NoSPIN,  //Means front-most rollers are not spinning
+  }
+
+  enum e_ArmShoulderPositionState {
+    IsUP,  //Means Elevator is in up postion
+    IsDOWN  // Means Elevator is in down position
+  }
+
+  enum e_ArmElbowPositionState {
+    IsPICKUP,   //Means Arms is in position to receive game element from Intake
+    IsHIGHCONE,  // Means Arm is in High Position to deposit cone
+    IsHIGHCUBE,  // Means Arm is in High Position to deposit cube
+    IsMIDCONE,  // Means Arm is in Mid Position to deposit cone
+    IsMIDCUBE,  // Means Arm is in Mid Position to deposit cube
+    IsFLOORCONE,  // Means Arm is in Floor High Position to deposit cone 
+    IsFLOORCUBE,  // Means Arm is in Floor Position to deposit cube
+  }
+
+  enum e_CurrentGameElementImCollecting{
+    CONE,
+    CUBE,
+    NONE
+  }
+
+  enum e_CurrentGameElementImPossessing{
+    CONE,
+    CUBE,
+    NONE
+  }
+
+
+  private RobotState(RobotStateIO io) {
+    this.io = io;
+  }
+
+
+  public static RobotState getInstance() {
+    if(instance == null) {instance = new RobotState(new RobotStateIO() {});}
+    return instance;
+  }
+
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.getInstance().processInputs("Intake", inputs);
+
+  }
+
+  @Override
+  public void simulationPeriodic() {}
+}
