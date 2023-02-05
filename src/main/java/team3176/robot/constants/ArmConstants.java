b914aa5e1fbf84dee8cf92d6fdd223ca279a8f36
char@odin <char3176@gmail.com>
2023-02-05 16:55:52 -0500

    Solved Christian's CAN/"NoFirmware" bug.

--- src/main/java/team3176/robot/constants/ArmConstants.java
+++ src/main/java/team3176/robot/constants/ArmConstants.java
@@ -10,8 +10,7 @@ package team3176.robot.constants;
  */
 public final class ArmConstants {
 
-    public static final int JOINT_MOTOR1_CAN_ID = 05; //TODO: Update CAN ID
-    public static final int ARM_SHOULDER_FALCON_CAN_ID = 50;
+    public static final int ARM_SHOULDER_FALCON_CAN_ID = 5;
     public static final int ARM_ELBOW_FALCON_CAN_ID = 50;
     public static final int ARM_WRIST_FALCON_CAN_ID = 50;
     public static final int limiter1Channel = 1;
