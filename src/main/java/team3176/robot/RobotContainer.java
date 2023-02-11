78bf7139b652592ed8e82319b8ff9d163f8d9fa7
Connor Custer <connor_da_bot@icloud.com>
2023-02-11 14:08:13 -0500

    path planner things

--- src/main/java/team3176/robot/RobotContainer.java
+++ src/main/java/team3176/robot/RobotContainer.java
@@ -22,6 +22,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import team3176.robot.commands.drivetrain.*;
 //import team3176.robot.commands.util.*;
 import team3176.robot.constants.LoggerConstants;
@@ -161,11 +162,11 @@ public class RobotContainer {
 
 
   public Command getAutonomousCommand() {
-    String chosen = m_autonChooser.getSelected();
+    //String chosen = m_autonChooser.getSelected();
 
     
-    PathPlannerAuto PPSwerveauto = new PathPlannerAuto();
-    return PPSwerveauto.getauto();
+    //PathPlannerAuto PPSwerveauto = new PathPlannerAuto();
+    return new WaitCommand(1.0);
   }
 
   
