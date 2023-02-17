e2f3644b565177ba418d0c4d4c400e51b73d68af
Connor Custer <connor_da_bot@icloud.com>
2023-02-16 20:13:23 -0500

    wait command causes error

--- src/main/java/team3176/robot/commands/drivetrain/PathPlannerAuto.java
+++ src/main/java/team3176/robot/commands/drivetrain/PathPlannerAuto.java
@@ -12,6 +12,7 @@ import com.pathplanner.lib.auto.SwerveAutoBuilder;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import team3176.robot.constants.DrivetrainConstants;
@@ -22,13 +23,13 @@ public class PathPlannerAuto {
     public PathPlannerAuto(String autoPathName) {
 
         Drivetrain driveSubsystem = Drivetrain.getInstance();
-        List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup(autoPathName, new PathConstraints(1.5, 1.5));
+        List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup(autoPathName, new PathConstraints(1.5, 0.5));
         System.out.println("length" + pathGroup.size());
         // This is just an example event map. It would be better to have a constant, global event map
         // in your code that will be used by all path following commands.
         HashMap<String, Command> eventMap = new HashMap<>();
         eventMap.put("print", new PrintCommand("action!"));
-        eventMap.put("cube", new WaitCommand(2.0));
+        eventMap.put("cube", new WaitCommand(5.0));
         // eventMap.put("intakeDown", new IntakeDown());
         // Create the AutoBuilder. This only needs to be created once when robot code starts, not every time you want to create an auto command. A good place to put this is in RobotContainer along with your subsystems.
         SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
@@ -36,14 +37,24 @@ public class PathPlannerAuto {
             driveSubsystem::resetPose,
             DrivetrainConstants.DRIVE_KINEMATICS,
             new PIDConstants(5.0,0.0,0.0),
-            new PIDConstants(0.5,0.0,0.0),
+            new PIDConstants(1.0,0.0,0.0),
             driveSubsystem::setModuleStates,
             eventMap,
             true,
             driveSubsystem);
 
-        autoBuilder.resetPose(pathGroup.get(0));
-        auto = autoBuilder.followPathWithEvents(pathGroup.get(0));
+        driveSubsystem.resetPose(pathGroup.get(0).getInitialHolonomicPose());
+
+        List<CommandBase> commands = new ArrayList<>();
+        commands.add(autoBuilder.resetPose(pathGroup.get(0)));
+    
+        for (PathPlannerTrajectory traj : pathGroup) {
+          //commands.add(autoBuilder.stopEventGroup(traj.getStartStopEvent()));
+          commands.add(autoBuilder.followPathWithEvents(traj));
+        }
+        //commands.add(autoBuilder.stopEventGroup(pathGroup.get(pathGroup.size() - 1).getEndStopEvent()));
+    
+        auto = new WaitCommand(2.0);//Commands.sequence(commands.toArray(CommandBase[]::new));//Commands.sequence(commands.toArray(CommandBase[]::new));
     }
     public Command getauto(){
         return auto;
